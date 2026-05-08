a6eafed1b0b1ef71d8c9029dd8274bf2b7a8a4e2
Rohit Shaw <rohitshaw0110@gmail.com>
2026-05-09 00:15:17 +0530

    Initial auth-service setup with MySQL configuration and User entity

---
+++ src/main/java/com/ecommerce/authservice/entity/User.java
@@ -0,0 +1,39 @@
+package com.ecommerce.authservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String phoneno;
+
+    @Column(nullable = false)
+    private String role;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+}
